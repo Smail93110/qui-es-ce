c2e00baac343328739dd9f19b3a4dbed1b97f951
Smail93110 <smailouldbey93pro@gmail.com>
2024-03-18 16:53:31 +0100

    jour 1

--- index.php
+++ index.php
@@ -3,9 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Qui-est-ce</title>
+    
+    <link href="index.css" rel="stylesheet" />
+
+    <link rel="icon" href="img/quiestce.png" height="200" width="20">
 </head>
 <body>
     
+    <h1> Qui est-ce  </h1>
+
+
+    <img src="./img/personnage/0000000.jpg" alt="image" id="1">
+    <img src="./img/personnage/0001111.jpg" alt="image">
+    <img src="./img/personnage/0010011.jpg" alt="image">
+    <img src="img/personnage/0011100.jpg" alt="image">
+    <img src="img/personnage/0101010.jpg" alt="image">
+    <img src="img/personnage/0110110.jpg" alt="image">
+   <img src="img/personnage/1001001.jpg" alt="image">
+   <img src="img/personnage/0100101.jpg" alt="image">
+   <img src="img/personnage/1000110.jpg" alt="image">
+   
+   <img src="./img/personnage/1010101.jpg" alt="image">
+   <img src="./img/personnage/1011010.jpg" alt="image">
+   <img src="./img/personnage/1100011.jpg" alt="image">
+   <img src="img/personnage/1101100.jpg" alt="image">
+   <img src="img/personnage/1110000.jpg" alt="image">
+   <img src="img/personnage/1111111.jpg" alt="image">
+  <img src="img/personnage/0111001.jpg" alt="image">
+
+  <h2 id="Q1">1. A-t-il des lunettes ?</h2>
+  <input id="b1O" type="button" value="Oui" onclick="oui(0)">
+  <input id="b1N" type="button" value="Non" onclick="non(1)"> </br> </br>
+
+
+
+  <input id="bouton" type="button" value="Cliquez pour avoir la réponse" onclick="affiche(m)">
 </body>
 </html>
